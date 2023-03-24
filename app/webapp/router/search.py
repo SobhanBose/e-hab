523d01de179cf7fa1f281069a324fd36782b63a0
Sobhan Bose <bosesobhan2003@gmail.com>
2023-03-24 18:25:23 +0530

    added distance filter

--- app/webapp/router/search.py
+++ app/webapp/router/search.py
@@ -1,7 +1,15 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, Depends
 from fastapi.templating import Jinja2Templates
-import requests, json
+from sqlalchemy.orm import Session
 from starlette.datastructures import URL
+from app.utils.database import get_db
+from app.utils import helpers
+from app.search_entities import responseModels
+from app.JWTtoken.token import SECRET_KEY, ALGORITHM
+from app import models
+from typing import List
+import geocoder
+from jose import jwt
 
 
 router = APIRouter(include_in_schema=False)
@@ -9,19 +17,38 @@ templates = Jinja2Templates(directory="app\\templates")
 templates.env.globals['URL'] = URL
 
 
-@router.get('/search-all')
-def search_all(request: Request):
-    entities = requests.get("http://127.0.0.1:8000/get_entities")
-    return templates.TemplateResponse("search.html", {"request": request, "entities": entities.json()})
-
-
 @router.get('/search-sg')
-def search_sg(request: Request):
-    entities = requests.get("http://localhost:8000/get_sg")
-    return templates.TemplateResponse("search.html", {"request": request, "entities": entities.json()})
+def search_sg(request: Request, db: Session = Depends(get_db)) -> List[responseModels.ShowEntity]:
+    access_token = request.cookies.get("access_token")
+    scheme, _, token = access_token.partition(" ")
+    payload = jwt.decode(token, SECRET_KEY, algorithms=ALGORITHM)
+    email = payload.get("sub")
+    user = db.query(models.Users).filter(models.Users.email == email).first()
+
+    entities = []
+    pos = geocoder.ip("me").latlng
+    sg = db.query(models.SupportGroups).all()
+    for entity in sg:
+        if helpers.calc_dist(pos, (entity.latitude, entity.longitude)) < 10:
+            entities.append(entity)
+
+    return templates.TemplateResponse("search.html", {"request": request, "entities": entities})
 
 
 @router.get('/search-rc')
-def search_rc(request: Request):
-    entities = requests.get("http://localhost:8000/get_rc")
-    return templates.TemplateResponse("search.html", {"request": request, "entities": entities.json()})
+def search_rc(request: Request, db: Session = Depends(get_db)) -> List[responseModels.ShowEntity]:
+    access_token = request.cookies.get("access_token")
+    scheme, _, token = access_token.partition(" ")
+    payload = jwt.decode(token, SECRET_KEY, algorithms=ALGORITHM)
+    email = payload.get("sub")
+    user = db.query(models.Users).filter(models.Users.email == email).first()
+
+    entities = []
+    rc = db.query(models.RehabCentres).all()
+    pos = geocoder.ip("me").latlng
+    entities.extend(list(rc))
+    for entity in rc:
+        if helpers.calc_dist(pos, (entity.latitude, entity.longitude)) < 10:
+            entities.append(entity)
+
+    return templates.TemplateResponse("search.html", {"request": request, "entities": entities})
